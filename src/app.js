831465172d72eaf1598d98626996477baa3103da
jingx <xin.jing1@thoughtworks.com>
2020-06-19 21:25:32 +0800

    [xin] initial the index

--- src/app.js
+++ src/app.js
@@ -1,1 +1,16 @@
 import "./style/index.scss";
+
+const apiTest = "http://localhost:8080/hello";
+
+export const getTest = async function () {
+  const result = fetch(apiTest, {
+    method: "GET",
+  })
+    .then((response) => {
+      return response.text();
+    })
+    .catch((err) => console.log(err));
+  return result;
+};
+
+// $('#test').html(getTest());
